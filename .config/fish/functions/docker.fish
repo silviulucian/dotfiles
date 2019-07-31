93cc7c393792650c28377bde0464e9417d192a61
Lucian Văcăroiu <silviulucian@gmail.com>
2019-07-31 23:53:06 +0300

    Updating Docker-related fish functions

--- .config/fish/functions/docker.fish
+++ .config/fish/functions/docker.fish
@@ -1,9 +1,13 @@
-function docker-stop-all
+function docker-kill-all
    docker kill (docker ps -q) # stop all containers
 end
 
-function docker-remove-all
+function docker-rm-all
    docker rm (docker ps -a -q) # remove all containers
    docker rmi (docker images -q) # remove all docker images
+end
+
+function docker-run
+   docker-compose run $argv
 end
 
