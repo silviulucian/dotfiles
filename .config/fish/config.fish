fa1a4b45f93c8c3b49909658170ef4c90a35921f
Lucian Văcăroiu <silviulucian@gmail.com>
2020-04-21 11:50:08 +0300

    Simplifying setup

--- .config/fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -gx LC_ALL en_US.UTF-8
-set -gx NVM_DIR $HOME/.nvm
-set -gx COMPOSER_DIR $HOME/.composer
-
-set -U fish_user_paths $COMPOSER_DIR/vendor/bin $NVM_DIR/bin $HOME/.yarn/bin $HOME/.config/yarn/global/node_modules/.bin $fish_user_paths
-
-nvm use default --silent
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
